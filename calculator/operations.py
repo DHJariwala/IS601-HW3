7a83eaef9175b5772ca585fa9ac8e56601958f42
dj332 <devjariwala76@gmail.com>
2025-02-11 00:21:42 -0500

    Added function docstring

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,15 +1,20 @@
+'''This module contains the basic arithmetic operations of a calculator'''
 from decimal import Decimal
-
+# Defining Functions with type hints
 def add(a: Decimal, b: Decimal) -> Decimal:
+    '''Add two numbers'''
     return a + b
 
 def subtract(a: Decimal, b: Decimal) -> Decimal:
+    '''Subtract two numbers'''
     return a - b
 
 def multiply(a: Decimal, b: Decimal) -> Decimal:
+    '''Multiply two numbers'''
     return a * b
 
 def divide(a: Decimal, b: Decimal) -> Decimal:
+    '''Divide two numbers. But raise an error if b is zero'''
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
