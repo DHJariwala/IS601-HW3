f49c3f82daf248f05273dd1f0d811183fc1e29c3
dj332 <devjariwala76@gmail.com>
2025-02-17 10:23:44 -0500

    Added docstring to calculator/calculation.py

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,17 +1,23 @@
-# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=line-too-long
+'''This document contains the Calculation class, which represents an arithmetic operation on two numbers.'''
 from decimal import Decimal
 from typing import Callable
 
 class Calculation:
+    '''This class represents an arithmetic operation on two numbers.'''
     def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        '''This function initializes the Calculation class.'''
         self.a = a
         self.b = b
         self.operation = operation
     @staticmethod
     def create(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        '''This function creates a Calculation object.'''
         return Calculation(a, b, operation)
 
     def perform(self) -> Decimal:
+        '''This function performs the arithmetic operation.'''
         return self.operation(self.a, self.b)
     def __repr__(self):
+        '''This function returns a string representation of the Calculation object.'''
         return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
